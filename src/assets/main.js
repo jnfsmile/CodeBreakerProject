7ab32e663c6295f693f9f69ba2a123c1f1484c62
JNF <jnfsmile@gmail.com>
2017-11-19 00:27:41 +0200

    stage 1

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,73 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (!answer || !attempt) {
+        setHiddenFields();
+    }
+    if (validateInput(input.value)) {
+        attempt++;
+    } else {
+        return false;
+    }
+
+    if (getResults(input.value)) {
+        setMessage("You Win! :)")
+        showAnswer(true);
+        showReplay();
+    } else if (attempt >= 10) {
+        setMessage("You Lose! :(")
+        showAnswer(false);
+        showReplay();
+    } else {
+        setMessage("Incorrect, try again.")
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+    answer = Math.floor(Math.random()*10000);
+    answer = answer.toString();
+    while (answer.length < 4) {
+        answer = "0" + answer;
+    }
+
+    attempt = 0;
+}
+
+function validateInput(input) {
+    if (input.length === 4) {
+        return true;
+    } else {
+        setMessage("Guesses must be exactly 4 characters long.");
+    }
+    return false;
+}
+
+function getResults(guess) {
+    let correct = 0;
+    let results = `<div class="row"><span class="col-md-6">${input}</span><div class="col-md-6">`
+    guess.split().forEach(element, i => {
+        if (element === answer[i] ) {
+            results += `<span class="glyphicon glyphicon-ok"></span>`;
+            correct++;
+        }
+        else if (answer.contains(element) ) results += `<span class="glyphicon glyphicon-transfer"></span>`
+        else results += `<span class="glyphicon glyphicon-remove"></span>` 
+    });
+    results += `</div></div>`;
+    let resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = results;
+    return correct === 4;
+}
+
+function showAnswer(win) {
+    let code = document.getElementById('code');
+    code.innerHTML = answer.value;
+    code.className += win ? " success" : " failure";
+}
+
+function showReplay() {
+    let guessing = document.getElementById("guessing-div");
+    guessing.style.display = "none";
+    let replay = document.getElementById("replay-div");
+    replay.style.display = "block";
+}
